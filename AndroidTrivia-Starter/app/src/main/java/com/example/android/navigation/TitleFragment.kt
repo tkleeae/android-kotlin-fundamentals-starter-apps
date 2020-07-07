dc753e4a3cb4dbe8418ec8bd76b32fd6230c9760
tkleeae <37610341+tkleeae@users.noreply.github.com>
2020-07-07 22:47:34 +0800

    share bundle through email

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -36,7 +36,7 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,container, false)
-        binding.playButton.setOnClickListener{view: View -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
+        binding.playButton.setOnClickListener{view: View -> view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())}
         return binding.root
     }
 
